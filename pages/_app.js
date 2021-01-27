c8cf9cd73de11f5d5068156746236dc409b86fc2
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-27 10:10:33 -0300

    feat(_app): added head to _app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { ThemeProvider } from 'styled-components';
 import db from '../db.json';
 import GlobalStyle from '../global';
@@ -5,6 +7,9 @@ import GlobalStyle from '../global';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>AluraQuiz</title>
+      </Head>
       <ThemeProvider theme={db.theme}>
         <GlobalStyle />
         <Component {...pageProps} />
